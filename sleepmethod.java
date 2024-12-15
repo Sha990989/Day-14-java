939610bd9b31bf3567bacb07b428e6d432bca1b3
A.Navaneetha <anavaneethasankar@gmail.com>
2024-12-15 11:41:45 +0530

    Add files via upload

---
+++ sleepmethod.java
@@ -0,0 +1,13 @@
+public class sleepmethod {
+    public static void main(String args[])
+    {
+        System.out.println("Thread started.....");
+        try{
+            Thread.sleep(5000);
+        }catch(InterruptedException e)
+        {
+            System.out.println("Interrupted");
+        }
+        System.out.println("Thread resumed");
+    }
+}
